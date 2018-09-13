eed9b9b17d4319d6c6d5f3886cb6313d2197260a
Devin Lee-Jones <devinleejones@gmail.com>
2018-09-13 11:11:55 -0700

    Created a new Car object, passing in the created $img and the other parements. #1

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+var $carImg = document.createElement('img')
+$carImg.setAttribute('src', 'http://res.cloudinary.com/devinleejones/image/upload/c_scale,w_88/v1536861812/images.png')
+document.body.appendChild($carImg)
+
 class Car {
   constructor($img, speed, direction, location) {
     this.$img = $img
@@ -6,3 +10,5 @@ class Car {
     this.location = location
   }
 }
+
+var myCar = new Car($img, 20, 'east', [0, 0])
